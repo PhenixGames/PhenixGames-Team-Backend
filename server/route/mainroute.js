2de5ccd5d8cdd8676a41721b71e410fb5c303bdd
Mittelblut9 <sauer.benedikt@outlook.de>
2021-08-10 01:34:18 +0200

    changed name & added (new) routes

---
+++ server/route/mainroute.js
@@ -0,0 +1,15 @@
+require("../init/file.init").fileinit(__filename, "init finished");
+const getuser = require("../../api/team/getuser/getuser");
+const nconf = require('nconf');
+const log = require("../../_log");
+const Status = require("../config/status.json");
+const {teamSignin} = require("../../api/team/signin/team-signin");
+const logout = require("../../api/team/logout/team-logout");
+const teamroute = nconf.get('mainrouting') + nconf.get('routing:team:main');
+
+module.exports = (app) => {
+    require('./sideroute/signin-route')(app, teamroute, nconf, teamSignin, log, Status)
+    require('./sideroute/getUser-route')(app, teamroute, nconf, getuser, log, Status)
+    require('./sideroute/logout-route')(app, teamroute, nconf, getuser, logout, Status)
+    require('./sideroute/player-route')(app, teamroute, nconf, log, Status)
+}
